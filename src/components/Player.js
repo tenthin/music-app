331052b6da00eb9a0e8ffd4b6eb93c452cda01f1
Tenzin <tenthinten@gmail.com>
2022-02-14 20:22:44 +0530

    to check if its playing

--- src/components/Player.js
+++ src/components/Player.js
@@ -3,9 +3,20 @@ import PlayerDetails from './PlayerDetails'
 import PlayerControls from './PlayerControls'
 
 function Player(props) {
+    const audioEl = useRef(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    useEffect(() => {
+        if (isPlaying) {
+            audioEl.current.play();
+        } else {
+            audioEl.current.pause();
+        }
+    })
+
   return (
     <div className="c-player">
-        <audio></audio>
+        <audio ref={audioEl}></audio>
         <h4>Playing now</h4>
         <PlayerDetails song={props.song}/>
         <PlayerControls/>
